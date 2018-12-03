4b1c841f2db23fae4024612caef8f5ba3f1aaadd
zhouzhuang2017 <zhouzhuang2017@163.com>
2018-12-03 21:34:53 +0800

    增加订单模块微服务

---
+++ transaction-order/src/main/java/com/zhou/transactionorder/web/OrderController.java
@@ -0,0 +1,17 @@
+/**
+ * Date:     2018/12/321:30
+ * AUTHOR:   Administrator
+ */
+package com.zhou.transactionorder.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 2018/12/3  21:30
+ * created by zhoumb
+ */
+@RestController
+@RequestMapping(value = "/api/order")
+public class OrderController {
+}
